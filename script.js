8909c1d6a6022f88ae0f2196c3ea4594c612045b
SiwyKrzysiek <k.fajny@gmail.com>
2019-07-21 14:53:18 +0200

    Drawing points

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
+const POINTS_COUNT = 20;
+let points = [];
 
 function setup() {
-
     createCanvas(700, 500).parent("sketch");
-    background(0);
+    background("gray");
+
+    points = createPoints();
 }
 
 function draw() {
-    circle(200, 200, 400);
+    // circle(200, 200, 400);
+
+    stroke(255)
+    fill(255)
+    for (const point of points) {
+        circle(point.x, point.y, 7);
+    }
+}
+
+function getPointCount() {
+    return POINTS_COUNT;
+}
+
+function createPoints() {
+    const padding = 40;
+
+    const points = [];
+
+    for (let i = 0, count = getPointCount(); i < count; i++) {
+        const x = random(padding, width - padding);
+        const y = random(padding, height - padding);
+
+        points.push(createVector(x, y));
+    }
+
+    return points;
 }
