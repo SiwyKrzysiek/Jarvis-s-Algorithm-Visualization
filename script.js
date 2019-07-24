77e0ac2370ee99d5abee3bfdc194216eb6744b8b
SiwyKrzysiek <k.fajny@gmail.com>
2019-07-24 18:18:24 +0200

    Drawing arrow

--- script.js
+++ script.js
@@ -2,21 +2,52 @@
 const POINTS_COUNT = 20;
 let points = [];
 
+let triangle = [];
+
 function setup() {
     createCanvas(700, 500).parent("sketch");
-    background("gray");
 
-    points = createPoints();
+    // points = createPoints();
 }
 
 function draw() {
+    background("gray");    
     // circle(200, 200, 400);
 
     stroke(255)
     fill(255)
-    for (const point of points) {
+    // for (const point of points) {
+    //     circle(point.x, point.y, 7);
+    // }
+
+    for (const point of triangle) {
         circle(point.x, point.y, 7);
     }
+
+    arrow(width / 2, 0, width / 2, 70);
+}
+
+function mousePressed() {
+    if (triangle.length >= 3)
+        triangle.length = 0;
+
+    triangle.push(createVector(mouseX, mouseY));
+}
+
+function arrow(x1, y1, x2, y2) {
+    let headLenght = 30;
+    const angle = atan2(y2 - y1, x2 - x1);
+
+    line(x1, y1, x2, y2);
+
+    let x = headLenght * cos(angle - PI / 4 + PI) + x2;
+    let y = headLenght * sin(angle - PI / 4 + PI) + y2;
+    line(x2, y2, x, y);
+
+    x = headLenght * cos(angle + PI / 4 + PI) + x2;
+    y = headLenght * sin(angle + PI / 4 + PI) + y2;
+    line(x2, y2, x, y);
+
 }
 
 function getPointCount() {
