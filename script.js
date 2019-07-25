82c09d0ead95b253003d4ac7e66dbc2c038c2845
SiwyKrzysiek <k.fajny@gmail.com>
2019-07-25 10:19:33 +0200

    Displaying orented triangle

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
+
 const POINTS_COUNT = 20;
 let points = [];
 
 let triangle = [];
+
+p5.Vector.prototype[Symbol.iterator] = function* () {
+    yield this.x;
+    yield this.y;
+};
+
+Array.prototype.last = () => this[this.length-1];
 
 function setup() {
     createCanvas(700, 500).parent("sketch");
@@ -11,7 +19,7 @@ function setup() {
 }
 
 function draw() {
-    background("gray");    
+    background("gray");
     // circle(200, 200, 400);
 
     stroke(255)
@@ -20,11 +28,11 @@ function draw() {
     //     circle(point.x, point.y, 7);
     // }
 
-    for (const point of triangle) {
-        circle(point.x, point.y, 7);
-    }
+    // for (const point of triangle) {
+    //     circle(point.x, point.y, 7);
+    // }
 
-    arrow(width / 2, 0, width / 2, 70);
+    displayPartialTriangle(triangle);
 }
 
 function mousePressed() {
@@ -34,20 +42,45 @@ function mousePressed() {
     triangle.push(createVector(mouseX, mouseY));
 }
 
+function displayPartialTriangle(triangle) {
+    const verexSize = 7;
+
+    if (triangle.length > 3)
+        throw new Error("Partial triangle can't have more than 3 points");
+
+    for (const point of triangle) {
+        circle(...point, verexSize);
+    }
+
+    for (let i = 1; i < triangle.length; i++) {
+        const prev = triangle[i - 1];
+        const curr = triangle[i];
+
+        arrow(...prev, ...curr);
+    }
+
+    if (triangle.length == 3)
+        arrow(...triangle[2], ...triangle[0]);
+}
+
 function arrow(x1, y1, x2, y2) {
-    let headLenght = 30;
+    let headLenght = 25;
+    const armsAngle = PI / 7;
+
+    const arrowLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     const angle = atan2(y2 - y1, x2 - x1);
+
+    if (headLenght > arrowLength / 3) 
+        headLenght = arrowLength / 3;
 
     line(x1, y1, x2, y2);
 
-    let x = headLenght * cos(angle - PI / 4 + PI) + x2;
-    let y = headLenght * sin(angle - PI / 4 + PI) + y2;
-    line(x2, y2, x, y);
-
-    x = headLenght * cos(angle + PI / 4 + PI) + x2;
-    y = headLenght * sin(angle + PI / 4 + PI) + y2;
-    line(x2, y2, x, y);
-
+    // Draw arms
+    for (let i of [1, -1]) {
+        let x = headLenght * cos(angle + i*armsAngle + PI) + x2;
+        let y = headLenght * sin(angle + i*armsAngle + PI) + y2;
+        line(x2, y2, x, y);
+    }
 }
 
 function getPointCount() {
